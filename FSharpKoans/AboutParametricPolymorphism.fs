a01df502883e4570f5569280f1748c21045022a0
g17c0339 <g17c0339@HUG184>
2019-03-23 15:58:42 +0200

    Stuck on 10.05

--- FSharpKoans/AboutParametricPolymorphism.fs
+++ FSharpKoans/AboutParametricPolymorphism.fs
@@ -65,22 +65,22 @@ module ``10: Parametric polymorphism`` =
         let a = false
         let b = 't'
         x |> should be ofType<int>
-        y |> should be ofType<FILL_ME_IN>
-        z |> should be ofType<FILL_ME_IN>
-        a |> should be ofType<FILL_ME_IN>
-        b |> should be ofType<FILL_ME_IN>
+        y |> should be ofType<string>
+        z |> should be ofType<float>
+        a |> should be ofType<bool>
+        b |> should be ofType<char>
 
     [<Test>]
     let ``02 id: the simplest built-in generic function`` () =
         // `id` is the identify function: it takes an input ... and gives it back immediately.
-        id 8 |> should equal __
-        id 7.6 |> should equal __
-        id "wut!" |> should equal __
+        id 8 |> should equal 8
+        id 7.6 |> should equal 7.6
+        id "wut!" |> should equal "wut!"
         // id can be surprisingly useful.  Remember it :).
 
     [<Test>]
     let ``03 Defining a generic function`` () =
-        let f x y = __
+        let f x y = (x, y, y)
         f 4 5 |> should equal (4, 5, 5)
         f "k" 'p' |> should equal ("k", 'p', 'p')
 
@@ -93,17 +93,17 @@ module ``10: Parametric polymorphism`` =
     }
     // we might create this with: { Something=5; Blah=8; Otherwise=9.3; What=77,"hi",0.88 }
 
-    type MyRecord = {
-        Who : FILL_ME_IN // <-- should be generic
-        What : FILL_ME_IN // <-- should be generic, and a different type to Who
+    type MyRecord <'a,'b> = {
+        Who : 'a // <-- should be generic
+        What : 'b // <-- should be generic, and a different type to Who
         Where : string
     }
 
     [<Test>]
     let ``04 Creating a generic record`` () =
         // You need to edit the definition of MyRecord first!  It's just above this test.
-        let a = __
-        let b = __  
+        let a = {Who = "The Doctor" ; What = 4.53 ; Where = "TTFN"}
+        let b = {Who = 'R'; What = false ; Where = "tiffin"}  
         a.Who |> should equal "The Doctor"
         b.Who |> should equal 'R'
         a.What |> should equal 4.53
@@ -121,8 +121,8 @@ module ``10: Parametric polymorphism`` =
         let a = Secnod (6.55, 7)
         let b = Thrid (fun k -> true, k, 8)
         // how do you write a generic type?
-        a |> should be ofType<FILL_ME_IN>
-        b |> should be ofType<FILL_ME_IN>
+        a |> should be ofType<float*int -> GenericDiscriminatedUnionExample<float, int>>
+        b |> should be ofType<('a -> 'd) -> bool*'a*int>
 
     type MyDiscriminatedUnion =
     | Furoth of FILL_ME_IN
